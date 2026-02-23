ba9660e6dc2987d261071563dc55d9345bd73b9f
gaurav <gauravdhall35@gmail.com>
2026-02-23 19:32:50 +0530

    build tested

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config({ path: "./server/.env" });
 const connectDB = require("./config/db.js");
 const testRoutes = require("./routes/testRoutes.js");
 const userRoutes = require("./routes/UserRoutes.js");
@@ -8,12 +10,22 @@ const cookieParser = require("cookie-parser");
 const tripRequestRoutes = require("./routes/TripRequestRoutes.js");
 const itineraryRoutes = require("./routes/ItineraryRoutes.js");
 
-dotenv.config();
+
 
 
 
 
 const app = express();
+
+const __dirnameResolved =__dirname;
+
+// static serve
+app.use(express.static(path.join(__dirnameResolved, "../client/dist")));
+
+// SPA fallback
+app.use((req, res) => {
+  res.sendFile(path.join(__dirnameResolved, "../client/dist/index.html"));
+});
 
 app.use((req, res, next) => {
   res.removeHeader("Access-Control-Allow-Origin");
@@ -39,13 +51,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/test",testRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/trip", tripRequestRoutes);
-app.use("/api/itinerary",itineraryRoutes)
-
-
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use("/api/itinerary",itineraryRoutes);
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "API working" });
 });
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
